0e99a7a0c1ba27dc69bdcf916d1d5f720ee346ab
agjayant <agjayant@rediff.com>
2017-01-31 16:29:40 +0530

    variable number of nodes for hidden layers

--- network.py
+++ network.py
@@ -10,6 +10,7 @@ initBias = 0.01
 class network:
     def __init__(self, nhidden_layers, nnodes, actfun):
 
+        assert(nhidden_layers == len(nnodes)), "Invalid Input, len(nnodes) vs nhidden_layers"
         self.nhid = nhidden_layers
         self.nnod = nnodes
         self.actfn = actfun
@@ -17,29 +18,32 @@ class network:
         ############################
 
         # input weights: connecting input layer and first hidden layer
-        self.inW =  np.random.uniform(minW,maxW,size=(nnodes, n_inputs +1 ) )
+        self.inW =  np.random.uniform(minW,maxW,size=(nnodes[0], n_inputs +1 ) )
         self.inW[:,n_inputs] = initBias
 
-        self.inError = np.asarray([[0 for i in range(n_inputs+1)] for j in range(nnodes)])
+        self.inError = np.asarray([[0 for i in range(n_inputs+1)] for j in range(nnodes[0])])
         # hidden weights: connecting intermediate hidden layers
         if nhidden_layers > 1:
-            self.hidW =  np.random.uniform(minW,maxW,size=(nhidden_layers - 1, nnodes,nnodes+1) )
-            self.hidW[:,:,nnodes] = initBias
-
-
-            self.hidError = np.asarray( [[[0 for i in range(nnodes+1)] for j in range(nnodes)] for k in range(nhidden_layers-1)] )
+            self.hidW = []
+            self.hidError = []
+            for i in range(nhidden_layers-1):
+                temp =  np.random.uniform(minW,maxW,size=( nnodes[i+1],nnodes[i]+1) )
+                self.hidW.append(temp)
+                self.hidW[i][:,nnodes[i]] = initBias
+                self.hidError.append(np.asarray([[0 for k in range(nnodes[i]+1)] for j in range(nnodes[i+1])]))
 
         # output weights: connecting last hidden layer and output layer
-        self.outW =  np.random.uniform(minW,maxW,size=(numClasses,nnodes+1) )
-        self.outW[:,nnodes] = initBias
+        self.outW =  np.random.uniform(minW,maxW,size=(numClasses,nnodes[nhidden_layers-1]+1) )
+        self.outW[:,nnodes[nhidden_layers-1]] = initBias
 
         #output weights accumulated Error
-        self.outError = np.asarray([[0 for i in range(nnodes+1)] for j in range(numClasses)])
+        self.outError = np.asarray([[0 for i in range(nnodes[nhidden_layers-1]+1)] for j in range(numClasses)])
 
         #each neuron has an input and an output
-        # netUnits[2][5][0], netUnits[2][5][1] --> input, output of the 6th neuron in 3rd layer
-        self.netUnits = [[[0 for inout in range(2)] for inlayer in range(nnodes)] for numlayers in range(nhidden_layers)]
-        self.netUnits = np.array(self.netUnits)
+        # netUnits[2][5,0], netUnits[2][5,1] --> input, output of the 6th neuron in 3rd layer
+        self.netUnits = []
+        for i in range(nhidden_layers):
+            self.netUnits.append(np.asarray([[0 for inout in range(2)] for inlayer in range(nnodes[i])]))
 
         # output of the network
         self.outNet = np.asarray([0]* numClasses)
@@ -57,20 +61,20 @@ class network:
         assert (self.nhid > 0), "Atleast One Hidden Layer"
 
         # computation for first hidden layer
-        self.netUnits[0,:,0] = np.dot(self.inW , inputVal)
-        self.netUnits[0,:,1] = self.activate(self.netUnits[0,:,0])
+        self.netUnits[0][:,0] = np.dot(self.inW , inputVal)
+        self.netUnits[0][:,1] = self.activate(self.netUnits[0][:,0])
 
         #computation for rest of the hidden layers
         if self.nhid > 1:
 
             for i in range(1,self.nhid):
-                tempInp = np.copy(self.netUnits[i-1,:,1])
+                tempInp = np.copy(self.netUnits[i-1][:,1])
                 tempInp = np.append(tempInp,1)
-                self.netUnits[i,:,0]= np.dot(self.hidW[i-1,:,:],tempInp)
-                self.netUnits[i,:,1]= self.activate(self.netUnits[i,:,0])
+                self.netUnits[i][:,0]= np.dot(self.hidW[i-1],tempInp)
+                self.netUnits[i][:,1]= self.activate(self.netUnits[i][:,0])
 
         #computation for the output layer
-        tempInp= np.copy(self.netUnits[self.nhid-1,:,1])
+        tempInp= np.copy(self.netUnits[self.nhid-1][:,1])
         tempInp = np.append(tempInp,1)
         self.outNet = np.dot(self.outW,tempInp)
         self.outProb = np.exp(self.outNet)/sum(np.exp(self.outNet))
@@ -82,18 +86,20 @@ class network:
         else:
             assert(0 == 1),'Invalid Activation Function'
 
-    def predict(self):
+    def predict(self, inputVal):
 
-        self.outProb = np.exp(self.outNet)/sum(np.exp(self.outNet))
+        self.forward(inputVal)
         return self.outProb.argmax()
 
     def update(self, batchSize,lr):
 
         self.inW = self.inW - lr*self.inError/batchSize
-        self.hidW = self.hidW - lr*self.hidError/batchSize
+        for i in range(self.nhid-1):
+            self.hidW[i] = self.hidW[i] - lr*self.hidError[i]/batchSize
         self.outW = self.outW - lr*self.outError/batchSize
 
     def backActivate(self, error, layer):
+        assert(len(self.netUnits[layer-1]) == len(error) ),"debugging 1"
 
         if self.actfn == 'relu':
 
@@ -102,7 +108,7 @@ class network:
 
             ReLU = np.vectorize(ReLU)
 
-            mask = ReLU(self.netUnits[layer-1,:,0])
+            mask = ReLU(self.netUnits[layer-1][:,0])
             return mask*error
         else:
             assert(0 == 1),'Invalid Activation Layer'
@@ -113,23 +119,22 @@ class network:
         assert(sum(label) == 1 ), "An image can not have more than one class"
 
         # gradient to be backpropagated for Cross Entropy Loss
-        # TODO correct this
         backError = self.outProb - label
 
         # errors for the last hidden layer
         lastHidError = []
 
-        for i in range(self.nnod):
+        for i in range(self.nnod[self.nhid - 1]):
             lastHidError.append( sum(backError * self.outW[:,i]) )
 
         # update output weights
         for i in range(numClasses):
-            for j in range(self.nnod):
-                self.outError[i,j] =  self.outError[i,j] + backError[i]*self.netUnits[self.nhid-1,j,1]
+            for j in range(self.nnod[self.nhid - 1]):
+                self.outError[i,j] =  self.outError[i,j] + backError[i]*self.netUnits[self.nhid-1][j,1]
 
         #update output biases
         for i in range(numClasses):
-            self.outError[i,self.nnod] = self.outError[i,self.nnod] + backError[i]
+            self.outError[i,self.nnod[self.nhid-1]] = self.outError[i,self.nnod[self.nhid-1]] + backError[i]
 
         prevError = np.asarray(lastHidError)
 
@@ -138,28 +143,28 @@ class network:
 
             tempError = self.backActivate(prevError , self.nhid - i)
             prevError = []
-            for j in range(self.nnod):
-                prevError.append(sum(tempError*self.hidW[self.nhid - i -2, :, j]))
+            for j in range(self.nnod[self.nhid-i-2]):
+                prevError.append(sum(tempError*self.hidW[self.nhid - i -2][:, j]))
 
             prevError = np.asarray(prevError)
             # update hidden weights
-            for k in range(self.nnod):
-                for j in range(self.nnod):
-                    self.hidError[ self.nhid - i-2,k,j ]= self.hidError[self.nhid -i-2,k,j] + tempError[j]*self.netUnits[self.nhid-i-2,k,1]
+            for k in range(self.nnod[self.nhid-i-2]):
+                for j in range(self.nnod[self.nhid-i-1]):
+                    self.hidError[ self.nhid - i-2][j,k ]= self.hidError[self.nhid -i-2][j,k] + tempError[j]*self.netUnits[self.nhid-i-2][k,1]
 
             # update hidden biases
-            for k in range(self.nnod):
-                self.hidError[self.nhid-i-2,k, self.nnod] = self.hidError[self.nhid-i-2,k, self.nnod] +tempError[k]
+            for k in range(self.nnod[self.nhid-i-1]):
+                self.hidError[self.nhid-i-2][k, self.nnod] = self.hidError[self.nhid-i-2][k, self.nnod] +tempError[k]
 
         tempError = self.backActivate(prevError, 1)
 
         #update input weights
-        for i in range(self.nnod):
+        for i in range(self.nnod[0]):
             for j in range(n_inputs):
                 self.inError[i, j] = self.inError[i,j] +  tempError[i]*inputVal[j]
 
         #update input biases
-        for i in range(self.nnod):
+        for i in range(self.nnod[0]):
             self.inError[i,n_inputs] = self.inError[i, n_inputs] + tempError[i]
 
     def trloss(self, inData, inlabel):
@@ -183,7 +188,8 @@ class network:
 
             #zero out the accumulated gradients
             self.inError[:,:] = 0
-            self.hidError[:,:,:] = 0
+            for k in range(self.nhid-1):
+                self.hidError[k][:,:] = 0
             self.outError[:,:] = 0
 
             loss = 0
